4b3f3f9468f560e4fc3d66f4288d2426fe5c3301
randolf segubre <rsegubre@gmail.com>
2024-03-09 22:43:24 +0800

    add rainfall service class for external http call and its depencies.

---
+++ Rainfall.Services/Interface/IRainfallDataService.cs
@@ -0,0 +1,10 @@
+﻿using Rainfall.Data;
+using Rainfall.Data.Interfaces;
+
+namespace Rainfall.Services.Interface
+{
+    public interface IRainfallDataService
+    {
+        Task<RainfallReadingResponse> GetRainfallDataAsync(string stationId, int count);
+    }
+}
