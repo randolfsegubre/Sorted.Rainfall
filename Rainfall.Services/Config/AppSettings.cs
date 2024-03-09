42a3e138e9bcd54c789ee5e6148b46aa4348c11c
randolf segubre <rsegubre@gmail.com>
2024-03-09 23:13:47 +0800

    refactoring to a cleaner code for rainfall service and app settings config.

---
+++ Rainfall.Services/Config/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rainfall.Services.Config
+{
+    public class AppSettings
+    {
+        public static string BaseUrl { get; set; }
+        public static string EndPointUrl { get; set; }
+    }
+}
--- Rainfall.Services/RainfallDataService.cs
+++ Rainfall.Services/RainfallDataService.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Rainfall.Data;
-using Rainfall.Data.Interfaces;
+﻿using Rainfall.Data;
+using Rainfall.Services.Config;
 using Rainfall.Services.Interface;
 using System.Net.Http.Json;
 
@@ -10,27 +8,25 @@ namespace Rainfall.Services
     public class RainfallDataService : IRainfallDataService
     {
         private readonly HttpClient _httpClient;
-        private readonly string _baseUrl;
-        private readonly string _endpointUrl;
 
-        public RainfallDataService(IConfiguration configuration)
+        public RainfallDataService()
         {
-            // Add required services
-            var services = new ServiceCollection();
-            services.AddAuthorizationCore(); // Add authorization services
-
-            // Create and configure HttpClient instance
             _httpClient = new HttpClient();
-            _baseUrl = configuration.GetSection("AppSettings:BaseUrl").Value ?? string.Empty;
-            _endpointUrl = configuration.GetSection("AppSettings:EndPointUrl").Value ?? string.Empty;
-            _httpClient.BaseAddress = new Uri(_baseUrl);
         }
 
+
+        /// <summary>
+        /// Method to get rainfall from external api
+        /// </summary>
+        /// <param name="stationId"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
         public async Task<RainfallReadingResponse> GetRainfallDataAsync(string stationId, int count)
         {
             // Construct the API endpoint URL
-            string formattedUri = _endpointUrl
-                    .Replace("{root}", _baseUrl)
+            string formattedUri = AppSettings.EndPointUrl
+                    .Replace("{root}", AppSettings.BaseUrl)
                     .Replace("{stationId}", stationId)
                     .Replace("{count}", count.ToString()) ;
 
